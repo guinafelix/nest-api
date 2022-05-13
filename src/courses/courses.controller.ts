18a9329fdd3003ec51679d63a57ae458ad16de1b
guilherme <oguilhermeleitefelix@gmail.com>
2022-05-13 18:53:52 -0300

    Create Courses Controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).send('listagem de serviços');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso #${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `atualização do curso ${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `exclusão do Curso #${id}`;
+  }
+}
