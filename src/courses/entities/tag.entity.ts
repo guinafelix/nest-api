be98c8d1fe137be199e12b835f4c99402130f43a
guilherme <oguilhermeleitefelix@gmail.com>
2022-05-15 08:56:03 -0300

    Create Tag Entity

---
+++ src/courses/entities/tag.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Course } from './course.entity';
+
+@Entity('tags')
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @ManyToMany(() => Course, (course: Course) => course.tags)
+  courses: Course[];
+}
